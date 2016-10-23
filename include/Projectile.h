24b3f6c48bd618a62c4e73e7e59548cb497b991b
Kevin Gentile <Kevin Gentile>
2016-10-22 22:44:55 -0400

    updated formatting of projectile class

--- include/Projectile.h
+++ include/Projectile.h
@@ -6,19 +6,9 @@
 class Projectile : public sf::CircleShape
 {
 	public:
-		Projectile(const sf::Vector2f& shipLocation){
-			this->setRadius(1.0);
-			this->setFillColor(sf::Color::White);
-			this->setPosition(shipLocation);
-			this->setOrigin(this->getLocalBounds().width/2, this->getLocalBounds().height/2);
-		}
-		~Projectile(){
-		}
-
-		void updateProjectile(){
-			this->move(0.0, -1.0);
-		}
-
+		Projectile(const sf::Vector2f& shipLocation);
+		~Projectile();
+		void updateProjectile();
 
 	private:
 		sf::Clock projectileClock;
--- src/Projectile.cpp
+++ src/Projectile.cpp
@@ -1,1 +1,14 @@
 #include "Projectile.h"
+
+Projectile::Projectile(const sf::Vector2f& shipLocation){
+    this->setRadius(1.0);
+    this->setFillColor(sf::Color::White);
+    this->setPosition(shipLocation);
+    this->setOrigin(this->getLocalBounds().width/2, this->getLocalBounds().height/2);
+}
+
+Projectile::~Projectile(){}
+
+void Projectile::updateProjectile(){
+    this->move(0.0, -1.0);
+}
